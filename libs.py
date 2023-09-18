0a099c4f00aa154105fefc3aecd8267f01cbe8b7
dominikstipic <dominikstipic@gmail.com>
2023-09-19 00:20:33 +0200

    version 10

--- libs.py
+++ libs.py
@@ -3,7 +3,7 @@ import subprocess
 def interactive_proc_start(cmd):
     cmds = cmd.strip().split()
     process = subprocess.Popen(cmds, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    while process.poll():
+    while process.poll() != None:
         output = process.stdout.readline()
         if output == b'': break
         print(output.decode('utf-8'))
